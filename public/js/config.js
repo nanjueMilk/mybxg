1692d65c157f6907cb0f23e4b8152616e95dcec4
liqing <1758523915@qq.com>
2017-09-20 16:19:36 +0800

    模板引擎重构登录功能

--- public/js/config.js
+++ public/js/config.js
@@ -1,9 +1,11 @@
 require.config({
-    baseUrl:'/public/assets',
-    paths:{
-        jquery:'jquery/jquery',
-        cookie:'jquery-cookie/jquery.cookie',
-        common:'../js/common',
-        login:'../js/login'
+    baseUrl: '/public/assets',
+    paths: {
+        jquery: 'jquery/jquery',
+        cookie: 'jquery-cookie/jquery.cookie',
+        template: 'artTemplate/template-web',
+        common: '../js/common',
+        login: '../js/login'
+
     }
 })
