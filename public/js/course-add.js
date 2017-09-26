b3d96844db6a1c46412385171ce96d4030a9b51b
liqing <1758523915@qq.com>
2017-09-26 23:53:10 +0800

    完成课程添加和编辑的页面共用

---
+++ public/js/course-add.js
@@ -0,0 +1,20 @@
+define(['jquery','util','form'], function ($,util) {
+    // 设置导航菜单栏选中
+    util.setMenu(location.pathname);
+    // 绑定表单提交单击事件
+    $('#courseBtn').click(function () {
+        $('#courseForm').ajaxSubmit({
+            type: 'post',
+            url: '/api/course/create',
+            //插件不需要传参
+            dataType: 'json',
+            success: function (data) {
+                if (data.code == 200) {
+                    //跳转到下一步
+                    //console.log(123)
+                    location.href = '/course/basic?cs_id=' + data.result.cs_id
+                }
+            }
+        })
+    })
+});
